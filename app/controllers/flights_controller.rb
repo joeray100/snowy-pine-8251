896293cc24152a3ff714112cceb2c77e011ce8d3
Joe Ray <jray100.jr@gmail.com>
2021-04-23 10:34:30 -0600

    take out pry

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,5 @@
 class FlightsController < ApplicationController
   def index
-    require 'pry'; binding.pry
     @flights = Flight.all
     # @airline = @flight.airline
     # @passengers = @flight.passengers
